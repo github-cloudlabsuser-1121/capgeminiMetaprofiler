860544464d66a3b82c27fc2b10b7a5ff9678f225
github_cloudlabsuser_1121 <github_cloudlabsuser_1121@cloudlabsaiuser.com>
2024-05-04 10:47:28 +0000

    committest

--- financeOpenAI.py
+++ financeOpenAI.py
@@ -1,1 +1,23 @@
-print("Hello")
+#Note: The openai-python library support for Azure OpenAI is in preview.
+#Note: This code sample requires OpenAI Python library version 1.0.0 or higher.
+import os
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+  azure_endpoint = "https://metaprofiler.openai.azure.com/", 
+  api_key=os.getenv("AZURE_OPENAI_KEY"),  
+  api_version="2024-02-15-preview"
+)
+
+message_text = [{"role":"system","content":"You are an AI assistant that helps people find information."}]
+
+completion = client.chat.completions.create(
+  model="Custome_service_sales", # model = "deployment_name"
+  messages = message_text,
+  temperature=0.7,
+  max_tokens=800,
+  top_p=0.95,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None
+)
